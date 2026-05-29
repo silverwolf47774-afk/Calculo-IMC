22642ec64f96c3431cedca271041d521a13a7b49
silverwolf47774-afk <silverwolf47774@gmail.com>
2026-05-29 12:49:50 +0100

    Sprint 2

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
-peso = float(input("Introduza o peso (kg): "))
-altura_cm = float(input("Introduza a altura (cm): "))
+def obter_dados():
+    peso = float(input("Introduza o peso (kg): "))
+    altura_cm = float(input("Introduza a altura (cm): "))
+    return peso, altura_cm
 
-altura_m = altura_cm / 100
 
-imc = peso / (altura_m * altura_m)
-imc = round(imc, 2)
+def converter_altura(altura_cm):
+    return altura_cm / 100
 
-print("IMC:", imc)
 
-if imc < 18.5:
-    print("Fora do peso normal")
-elif imc < 25:
-    print("Peso normal")
-else:
-    print("Fora do peso normal")
+def calcular_imc(peso, altura_m):
+    return peso / (altura_m * altura_m)
+
+
+# imc = Índice de Massa Corporal
+# tdp = Tipo de Peso
+
+def classificar_imc(imc):
+    if imc < 18.5:
+        return "Abaixo do peso"
+    elif imc < 25:
+        return "Peso normal"
+    elif imc < 30:
+        return "Excesso de peso"
+    else:
+        return "Obesidade"
+
+def main():
+    peso, altura_cm = obter_dados()
+    altura_m = converter_altura(altura_cm)
+
+    imc = calcular_imc(peso, altura_m)
+    imc = round(imc, 2)
+
+    tdp = classificar_imc(imc)
+
+    print("Classificação:", tdp)
+
+
+main()
